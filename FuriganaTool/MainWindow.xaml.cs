1d2c7aa86753921e2476bf3b5bceb4e1b02961e6
Showmik Haque <showmik.haque@gmail.com>
2021-09-01 11:47:55 +0600

    Row count number text added

--- FuriganaTool/MainWindow.xaml.cs
+++ FuriganaTool/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ namespace FuriganaTool
     {
         private List<TextBox> kanjiTextBoxList = new List<TextBox>();
         private List<TextBox> furiganaTextBoxList = new List<TextBox>();
+        private List<TextBlock> rowCounterTexBlockList = new List<TextBlock>();
         private int rowCounter;
 
         public MainWindow()
@@ -65,24 +66,43 @@ namespace FuriganaTool
         private void AddRowButton_Click(object sender, RoutedEventArgs e)
         {
             rowCounter++;
-            kanjiTextBoxList.Add(new TextBox());
-            Grid.SetRow(kanjiTextBoxList.Last(), rowCounter - 1);
-            Grid.SetColumn(kanjiTextBoxList.Last(), 0);
-            kanjiTextBoxList.Last().Height = 30;
-            kanjiTextBoxList.Last().Margin = new Thickness(5);
+            TextBoxGrid.RowDefinitions.Add(new RowDefinition());
 
+            AddRowCounterTextBlock();
+            AddKanjiTextBox();
+            AddFutiganaTextBox();
+        }
+
+        private void AddRowCounterTextBlock()
+        {
+            rowCounterTexBlockList.Add(new TextBlock());
+            Grid.SetRow(rowCounterTexBlockList.Last(), rowCounter - 1);
+            Grid.SetColumn(rowCounterTexBlockList.Last(), 0);
+            rowCounterTexBlockList.Last().Text = rowCounter.ToString();
+
+            TextBoxGrid.Children.Add(rowCounterTexBlockList.Last());
+        }
+
+        private void AddFutiganaTextBox()
+        {
             furiganaTextBoxList.Add(new TextBox());
             Grid.SetRow(furiganaTextBoxList.Last(), rowCounter - 1);
-            Grid.SetColumn(furiganaTextBoxList.Last(), 1);
+            Grid.SetColumn(furiganaTextBoxList.Last(), 2);
             furiganaTextBoxList.Last().Height = 30;
             furiganaTextBoxList.Last().Margin = new Thickness(5);
-
-            kanjiTextBoxList[rowCounter - 1].Name = $"TextBoxR{rowCounter - 1}C0";
-            furiganaTextBoxList[rowCounter - 1].Name = $"TextBoxR{rowCounter - 1}C1";
-
-            TextBoxGrid.RowDefinitions.Add(new RowDefinition());
-            TextBoxGrid.Children.Add(kanjiTextBoxList.Last());
             TextBoxGrid.Children.Add(furiganaTextBoxList.Last());
+            furiganaTextBoxList[rowCounter - 1].Name = $"TextBoxR{rowCounter - 1}C2";
+        }
+
+        private void AddKanjiTextBox()
+        {
+            kanjiTextBoxList.Add(new TextBox());
+            Grid.SetRow(kanjiTextBoxList.Last(), rowCounter - 1);
+            Grid.SetColumn(kanjiTextBoxList.Last(), 1);
+            kanjiTextBoxList.Last().Height = 30;
+            kanjiTextBoxList.Last().Margin = new Thickness(5);
+            TextBoxGrid.Children.Add(kanjiTextBoxList.Last());
+            kanjiTextBoxList[rowCounter - 1].Name = $"TextBoxR{rowCounter - 1}C1";
         }
 
         private void CopyButton_Click(object sender, RoutedEventArgs e)
@@ -93,18 +113,18 @@ namespace FuriganaTool
 
         private void InitializeKanjiTextBoxList()
         {
-            kanjiTextBoxList.Add(TextBoxR0C0);
-            kanjiTextBoxList.Add(TextBoxR1C0);
-            kanjiTextBoxList.Add(TextBoxR2C0);
-            kanjiTextBoxList.Add(TextBoxR3C0);
+            kanjiTextBoxList.Add(TextBoxR0C1);
+            kanjiTextBoxList.Add(TextBoxR1C1);
+            kanjiTextBoxList.Add(TextBoxR2C1);
+            kanjiTextBoxList.Add(TextBoxR3C1);
         }
 
         private void InitializeFuriganaTextBoxList()
         {
-            furiganaTextBoxList.Add(TextBoxR0C1);
-            furiganaTextBoxList.Add(TextBoxR1C1);
-            furiganaTextBoxList.Add(TextBoxR2C1);
-            furiganaTextBoxList.Add(TextBoxR3C1);
+            furiganaTextBoxList.Add(TextBoxR0C2);
+            furiganaTextBoxList.Add(TextBoxR1C2);
+            furiganaTextBoxList.Add(TextBoxR2C2);
+            furiganaTextBoxList.Add(TextBoxR3C2);
         }
     }
 }
